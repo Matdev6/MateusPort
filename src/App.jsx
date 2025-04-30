cd2235769a182e3e257cb9b28d5a4ee365f27a4b
Matdev6 <mateusousa123@hotmail.com>
2025-04-30 14:03:25 -0300

    loaded state
    await page loaded

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
 import Header from "./components/Header"
 import Home from "./pages/Home"
-import Login from "./pages/Login"
-
+import { useEffect, useState } from "react"
 
 function App() {
+  const [isLoaded, setIsLoaded] = useState(false)
 
+  useEffect(() => {
+    const handleLoaded = () => {
+      setIsLoaded(true)
+    };
+
+    window.addEventListener('load', handleLoaded)
+
+    return() => {
+      window.removeEventListener('load', handleLoaded)
+    }
+
+  }, [])
+
+  if(!isLoaded) {
+    return <div className="">Carregando...</div>
+  }
 
   return (
     <div className="overflow-hidden scrollbar-thin scrollbar-thumn-cyan-500 scrollbar-track">
