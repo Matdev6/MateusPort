b838df88993d9c7a443636d1a98f89f9eb0a06bf
Matdev6 <mateusousa123@hotmail.com>
2025-04-30 14:11:08 -0300

    fix loaded
    fix loaded

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,35 @@
+import { useEffect, useState } from "react"
 import Header from "./components/Header"
 import Home from "./pages/Home"
-import { useEffect, useState } from "react"
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false)
 
   useEffect(() => {
-    const handleLoaded = () => {
+    const handleLoad = () => {
+      console.log("Página carregada!")
       setIsLoaded(true)
     }
 
-    window.addEventListener('load', handleLoaded)
+    // Se a página já estiver carregada
+    if (document.readyState === "complete") {
+      setIsLoaded(true)
+    } else {
+      window.addEventListener("load", handleLoad)
+    }
 
     return () => {
-      window.removeEventListener('load', handleLoaded)
+      window.removeEventListener("load", handleLoad)
     }
   }, [])
 
   if (!isLoaded) {
     return (
-      <div className="flex items-center justify-center h-screen bg-black text-white text-xl">
-        Carregando...
+      <div className="flex items-center justify-center h-screen bg-black text-white">
+        <div className="flex flex-col items-center space-y-4">
+          <div className="w-10 h-10 border-4 border-cyan-500 border-dashed rounded-full animate-spin"></div>
+          <span className="text-lg">Carregando...</span>
+        </div>
       </div>
     )
   }
